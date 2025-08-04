cc602dce6e06fa2d0a7ba2f6e031a500b0bc1e3a
bryantfriend <72289778+bryantfriend@users.noreply.github.com>
2025-08-04 15:56:57 +0600

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,15 +1,18 @@
 // js/firebase-init.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// --- VERSION UPDATED FOR CONSISTENCY ---
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBJYVVwZvPGWezU0nPkfPQE8wpnElRZTIU",
   authDomain: "mycafe-39b53.firebaseapp.com",
   projectId: "mycafe-39b53",
   storageBucket: "mycafe-39b53.appspot.com",
-  messagingSenderId: "11006381908561",
-  appId: "1:11006381908561:web:d3a911adc3259ea4fc2e70"
+  // --- Double-check this value in your Firebase console ---
+  messagingSenderId: "110063819085", // Example of a 12-digit ID
+  appId: "1:110063819085:web:d3a911adc3259ea4fc2e70"
 };
 
 const app = initializeApp(firebaseConfig);
